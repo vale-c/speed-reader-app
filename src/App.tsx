cd065a0d875f6faf3a18f95d74f4549a10d88af3
Valentina Calabrese <valentina.calabrese@remote.com>
2025-02-28 21:18:46 +0100

    (chore): remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
 import { StoreProvider, useSpeedReaderStore } from '@/stores/StoreProvider';
-import SpeedReader from '@/components/SpeedReader';
 import WordDisplay from '@/components/WordDisplay';
 import ReadingControls from '@/components/ReadingControls';
 import TextInput from '@/components/TextInput';
